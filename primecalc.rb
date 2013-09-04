9695cfc94aafc8d3950766d02926cc9d98b7bdf0
Alex Savin <karismafilms@gmail.com>
2013-09-04 13:45:59 +0300

    All methods are covered with tests now

--- primecalc.rb
+++ primecalc.rb
@@ -10,83 +10,83 @@
 #
 
 class PrimeCalc
-def calculatePrimesUpTo(max)
-  if validatePrimeInput(max)
-    # Initializing array with the first prime number
-    myPrimes = [2]
+  def calculatePrimesUpTo(max)
+    if validatePrimeInput(max)
+      # Initializing array with the first prime number
+      myPrimes = [2]
 
-    # Initialize array with potential candidates
-    # Only take odd numbers starting from 3
-    allCandidates = (3..max).step(2).to_a
+      # Initialize array with potential candidates
+      # Only take odd numbers starting from 3
+      allCandidates = (3..max).step(2).to_a
 
-    while !allCandidates.empty? do
-      myPrimes << allCandidates.first
-      puts "Discovered a new prime: " + myPrimes.last.to_s
-      allCandidates.delete(myPrimes.last)
+      while !allCandidates.empty? do
+        myPrimes << allCandidates.first
+        puts "Discovered a new prime: " + myPrimes.last.to_s
+        allCandidates.delete(myPrimes.last)
 
-      # Removing all candidates divisible by last calculated prime
-      allCandidates.each do |i|
-        if i % myPrimes.last == 0
-          allCandidates.delete(i)
+        # Removing all candidates divisible by last calculated prime
+        allCandidates.each do |i|
+          if i % myPrimes.last == 0
+            allCandidates.delete(i)
+          end
         end
       end
+
+      myPrimes
+    else
+      puts "Wrong max input"
     end
-
-    myPrimes
-  else
-    puts "Wrong max input"
   end
-end
 
-# Only accepting fixnums greater than 2
-def validatePrimeInput(input)
-  if (input.is_a? Fixnum) and input > 2
-    true
-  else
-    false
+  # Only accepting fixnums greater than 2
+  def validatePrimeInput(input)
+    if (input.is_a? Fixnum) and input > 2
+      true
+    else
+      false
+    end
   end
-end
 
-#
-# This method takes an array, multiply every value
-# and prints results into a neat table
-#
-def printMultiplicationTable(numArray)
-  puts "\nPrime numbers multiplication table"
+  #
+  # This method takes an array, multiply every value
+  # and prints results into a neat table
+  #
+  def printMultiplicationTable(numArray)
+    puts "\nPrime numbers multiplication table"
   
-  # Printing beautified first row with initial margin
-  firstRow = "        "
-  numArray.each do |i|
-    firstRow += alignTabularValue(i.to_s) + " | "
-  end
+    # Printing beautified first row with initial margin
+    firstRow = "        "
+    numArray.each do |i|
+      firstRow += alignTabularValue(i.to_s) + " | "
+    end
   
-  puts firstRow
+    puts firstRow
   
-  numArray.each do |i|
-    printString = alignTabularValue(i.to_s) + ' | '
-    numArray.each do |ii|
-      result = (i * ii).to_s
+    numArray.each do |i|
+      printString = alignTabularValue(i.to_s) + ' | '
+      numArray.each do |ii|
+        result = (i * ii).to_s
       
-      printString += alignTabularValue(result) + ' | '
+        printString += alignTabularValue(result) + ' | '
+      end
+      puts printString
     end
-    puts printString
-  end
   
-  puts "\n"
-end
+    puts "\n"
+  end
 
-def alignTabularValue(value)
-  # A constant for table column width
-  tabularWidth = 5
+  def alignTabularValue(value)
+    # A constant for table column width
+    tabularWidth = 5
   
-  # Aligning table columns
-  if value.length < tabularWidth
-    padding = tabularWidth - value.length
-    padding.times do
-      value += " "
+    # Aligning table columns
+    if value.length < tabularWidth
+      padding = tabularWidth - value.length
+      padding.times do
+        value += " "
+      end
     end
-  end
   
-  value
-end
+    value
+  end
 end
--- spec/primecalc_spec.rb
+++ spec/primecalc_spec.rb
@@ -1,9 +1,37 @@
 require "spec_helper"
 require_relative "../primecalc"
 
+# To compare our results with Ruby implementation of Prime
+require "prime"
+
 describe "A primes calculation" do
-  it "doesn't validate 1 for max input" do  
-    primeCalc = new PrimeCalc
-    primeCalc.validatePrimeInput(1).should == false
+  before :each do
+    @primeCalc = PrimeCalc.new
+  end
+  
+  it "doesn't approve string for max input" do  
+    @primeCalc.validatePrimeInput('aaa').should be_false
+  end
+  
+  it "doesn't approve fixnum less than 3 for max input" do  
+    @primeCalc.validatePrimeInput(2).should be_false
+  end
+  
+  it "calculates all primes for the given max value" do
+    ourArray = @primeCalc.calculatePrimesUpTo(100)
+    
+    # Calculating primes with Ruby built-in class
+    # for comparison
+    rubyPrimeArray = Prime.take(ourArray.length)
+    ourArray.should == rubyPrimeArray
+  end
+  
+  it "prints multiplication array" do
+    ourArray = @primeCalc.calculatePrimesUpTo(100)
+    @primeCalc.printMultiplicationTable(ourArray).should_not raise_error()
+  end
+  
+  it "properly aligns tabular value" do
+    @primeCalc.alignTabularValue('2').should == '2    '
   end
 end
